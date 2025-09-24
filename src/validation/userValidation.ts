09fc7e5bae780d06360d1effd0ab5bef614c82cd
Anup Maurya <anup.maurya484@gmail.com>
2025-09-24 12:37:36 +0530

    bug fix

--- src/validation/userValidation.ts
+++ src/validation/userValidation.ts
@@ -2,7 +2,7 @@
 const Joi = require('joi');
 
 export const registerJoiSchema = Joi.object({
-    user_name: Joi.string().min(3).required(),
+    username: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(2).required(),
     confirm_password: Joi.string().min(2).required(),
